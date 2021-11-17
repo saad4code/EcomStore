9db519cec6501a1c66b568916bd3b9aeec52d056
CleverCodder <m.saad4code@gmail.com>
2021-11-17 14:27:26 +0500

    First Commit

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -10,7 +10,7 @@ const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart
   const EmptyCart = () => (
 
     <Typography variant="subtitle1">You have no items in your shopping cart,
-        <Link to="/" className={classes.link}> adding some</Link>!
+        <Link to="/" className={classes.link}> adding </Link>!
     </Typography>
 
   )
