8945aad4573cbf9adedacbeedac353a32c1ce2d6
CleverCodder <m.saad4code@gmail.com>
2021-11-17 14:22:46 +0500

    Deploy App

---
+++ src/components/Cart/CartItem/styles.js
@@ -0,0 +1,23 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles(() => ({
+	media: {
+		height: 260,
+	},
+	cardContent: {
+		display: "flex",
+		justifyContent: "space-between",
+	},
+	cartActions: {
+		justifyContent: "space-between",
+	},
+	buttons: {
+		display: "flex",
+		alignItems: "center",
+	},
+	spinner: {
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+}));
---
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useFormContext, Controller } from 'react-hook-form';
+import { TextField, Grid } from '@material-ui/core';
+
+const FormInput = ({ name, label }) => {
+
+    const { control } = useFormContext();
+    const isError = false;
+    
+  return (
+     <Grid item xs={12} sm={6}>
+           <Controller as={TextField} defaultValue="" name={name} control={control} label={label} fullWidth required error={isError}/>
+     </Grid>
+  )
+}
+
+export default FormInput
