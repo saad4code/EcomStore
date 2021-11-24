9f75a939cd785e90ed07cd2c10e0a00eb01ff343
CleverCodder <m.saad4code@gmail.com>
2021-11-24 12:27:43 +0500

    Testing

--- src/components/CheckoutForm/CustomTextField.jsx
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { useForm , Controller} from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { TextField, Grid } from '@material-ui/core';
 
 
 const FormInput = ({ name, label }) => {
 
-    const { control } = useForm ();
-    const isError = false;
-    
+  const { control } = useForm();
+  const isError = false;
+
   return (
-     <Grid item xs={12} sm={6}>
-           <Controller as={TextField} defaultValue="" name={name} control={control} label={label} fullWidth required error={isError}/>
-     </Grid>
+    <Grid item xs={12} sm={6}>
+      <Controller as={TextField} defaultValue="" name={name} control={control} label={label} fullWidth required error={isError} />
+    </Grid>
   )
 }
 
