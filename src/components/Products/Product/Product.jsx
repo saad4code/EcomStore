b1daeb1ebba8a13a3db5c8f39db4916d0795b9e0
CleverCodder <m.saad4code@gmail.com>
2022-01-19 12:53:57 +0500

    Code correct

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,59 +1,60 @@
-import React, { useState ,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton, CircularProgress } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
 import useStyles from './style';
 
 const Product = ({ product, onAddToCart, cartLoading }) => {
-	const [currentClicked, setCurrentClicked] = useState(``);
+  const [currentClicked, setCurrentClicked] = useState(``);
 
   useEffect(() => {
-    if(!cartLoading){
+    if (!cartLoading) {
       setCurrentClicked(``)
     }
   }, [cartLoading])
 
   const classes = useStyles();
- console.log(currentClicked )
+  // console.log(currentClicked)
   return (
-      <Card className={classes.root}>
-        
-        <CardMedia className={classes.media} image={product.image.url} title={product.name} />
-        <CardContent>
+    <Card className={classes.root}>
 
-          <div className={classes.cardContent}>
-            <Typography gutterBottom variant="h5">
-               {product.name}
-            </Typography>
-             <Typography variant="h5">
-               {product.price.formatted_with_symbol}
-            </Typography>
-          </div>
+      <CardMedia className={classes.media} image={product.image.url} title={product.name} />
+      <CardContent>
 
-          <Typography dangerouslySetInnerHTML={{__html: product.description}} variant="body2" color="textSecondary" />
+        <div className={classes.cardContent}>
+          <Typography gutterBottom variant="h5">
+            {product.name}
+          </Typography>
+          <Typography variant="h5">
+            {product.price.formatted_with_symbol}
+          </Typography>
+        </div>
 
-        </CardContent>
+        <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" />
 
-        <CardActions disableSpacing className={classes.cardActions}>
-         {
+      </CardContent>
+
+      <CardActions disableSpacing className={classes.cardActions}>
+        {
           currentClicked !== product.id ? (
-            
-           <IconButton aria-label="Add to Cart" onClick={() => {
-             setCurrentClicked(product.id)
-             onAddToCart(product.id, 1)}}>
-              <AddShoppingCart />   
-           </IconButton>
-          ): (
-             <div className={classes.spinner}>
+
+            <IconButton aria-label="Add to Cart" onClick={() => {
+              setCurrentClicked(product.id)
+              onAddToCart(product.id, 1)
+            }}>
+              <AddShoppingCart />
+            </IconButton>
+          ) : (
+            <div className={classes.spinner}>
               <CircularProgress />
-             </div>
-            
+            </div>
+
           )
         }
-          
-         
-        </CardActions>
-      </Card>
-      )
+
+
+      </CardActions>
+    </Card>
+  )
 }
 
 export default Product
