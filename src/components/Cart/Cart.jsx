900edfa9abe80a9be5c832078563b59aa63a81d2
CleverCodder <m.saad4code@gmail.com>
2022-01-19 12:43:55 +0500

    Page Fix

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -10,37 +10,35 @@ const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart
   const EmptyCart = () => (
 
     <Typography variant="subtitle1">You have no items in your shopping cart,
-        <Link to="/" className={classes.link}> adding some</Link>!
+      <Link to="/" className={classes.link}> adding some</Link>!
     </Typography>
 
   )
 
   const FilledCart = () => !isLoading ? (
     <>
-    <Grid container spacing={4}  xs={12} sm={12} >
-      <Grid container  spacing={4} item xs={8} sm={10}>
-        {cart.line_items.map((item) => (
-          <Grid item xs={12} sm={5} key={item.id}>
-            <CartItem item={item} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} />
-          </Grid>
-        ))}
+      <Grid container item spacing={4} xs={12} sm={12}>
+        <Grid container item spacing={4} xs={8} sm={10}>
+          {cart.line_items.map((item, ind) => (
+            <Grid key={ind} item xs={12} sm={5}>
+
+              <CartItem item={item} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} />
+
+            </Grid>
+          ))}
+        </Grid>
+        <Grid container item xs={4} sm={2}>
+          <div className={classes.cardDetails}>
+            <Typography variant="h4">Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
+
+            <Button style={{ marginTop: "12px" }} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart}>Empty cart</Button>
+            <Button style={{ color: "white", marginTop: "10px" }} component={Link} to="/checkout" className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
+
+          </div>
+        </Grid>
       </Grid>
-      <Grid xs={4} sm={2}>
-         <div className={classes.cardDetails}>
 
-    <Typography variant="h4">Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
 
-          <Button style={{ marginTop:"12px"}} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart}>Empty cart</Button>
-
-          
-          <Button style={{color:"white", marginTop:"10px"}} component={Link} to="/checkout" className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
-       
-
-    </div>
-      </Grid>
-    </Grid>
-
-   
     </>
 
   ) : (
@@ -51,16 +49,16 @@ const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart
     </div>
   );
 
-  if(!cart.line_items) return "Loading..."
+  if (!cart.line_items) return "Loading..."
 
   return (
     <Container>
 
       <div className={classes.toolbar}>
         <Typography className={classes.title} variant="h3" gutterBottom>Your Shopping Cart</Typography>
-        { !cart.line_items.length ? <EmptyCart /> : <FilledCart /> }
+        {!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
       </div>
-      
+
     </Container>
   )
 }
